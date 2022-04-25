d86c257b0f49f603f6a21bea475224efd61786a5
imane <rkhisimane02@gmail.com>
2022-04-25 10:25:51 +0000

    first commit

---
+++ src/routes/client.routes.js
@@ -0,0 +1,10 @@
+const client = require('../controllers/client.controller')
+const express = require('express')
+const router = express.Router()
+
+router.post('/', client.authenticate);
+router.post('/phoneBill', client.phoneBill);
+router.post('/carTax', client.carTax);
+router.post('/ticket', client.ticket);
+
+module.exports = router
